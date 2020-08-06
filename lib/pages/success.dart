93031add9ef35176b614152a1cdc01dd9b4c5983
Aguilaair <eduardomoreno.eduardo@gmail.com>
2020-08-06 18:22:00 +0200

    finalise success page

--- lib/pages/success.dart
+++ lib/pages/success.dart
@@ -1,146 +1,102 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
-import '../widget/textLogin.dart';
-import '../widget/verticalText.dart';
+import 'package:gategoDeploy/pages/deployPage.dart';
+import 'package:gategoDeploy/widget/bottomNavBar.dart';
 
 class SuccessPage extends StatelessWidget {
   final String ip;
   final String bssid;
-
   SuccessPage(this.ip, this.bssid);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "assets/gatego.png",
+          height: 30,
+        ),
+      ),
+      bottomNavigationBar: BottomNavBar(
+        DeployPage(),
+        backBtn: false,
+        textMain: "Re-deploy ",
+        iconMain: Icons.play_for_work,
+        goHome: true,
+      ),
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [Color(0xff00F260), Color(0xff00A9DE)]),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                "assets/gatego.png",
-                width: MediaQuery.of(context).size.width * 0.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 20,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              height: MediaQuery.of(context).size.width * 0.4,
+              child: FlareActor(
+                "assets/success.flr",
+                alignment: Alignment.center,
+                fit: BoxFit.contain,
+                isPaused: false,
+                animation: "Untitled",
               ),
-              Row(
-                children: <Widget>[
-                  VerticalText(),
-                  TextLogin(),
-                ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Successfully set up device.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context).accentColor,
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
               ),
-              SizedBox(
-                height: 5,
-              ),
-              Icon(
-                Icons.done_all,
-                size: 100,
-                color: Colors.white,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Successfully set wifi up.",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                    fontWeight: FontWeight.w700),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.width * 0.4,
-                width: MediaQuery.of(context).size.width * 0.8,
-                child: Card(
-                  shadowColor: Colors.blue[300],
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Padding(
-                    padding: EdgeInsets.all(30),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              Icons.wifi,
-                              size: 30,
-                            ),
-                            Text(ip)
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Icon(
-                              Icons.all_out,
-                              size: 30,
-                            ),
-                            Text(bssid)
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 10, right: 50, left: 100),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.blue[300],
-                        blurRadius:
-                            10.0, // has the effect of softening the shadow
-                        spreadRadius:
-                            1.0, // has the effect of extending the shadow
-                        offset: Offset(
-                          5.0, // horizontal, move right 10
-                          5.0, // vertical, move down 10
-                        ),
-                      ),
-                    ],
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          'Re-Deploy',
-                          style: TextStyle(
-                            color: Colors.lightBlueAccent,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w700,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.width * 0.4,
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.wifi,
+                            size: 30,
                           ),
-                        ),
-                        Icon(
-                          Icons.play_for_work,
-                          color: Colors.lightBlueAccent,
-                        ),
-                      ],
-                    ),
+                          Text(ip)
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Icon(
+                            Icons.all_out,
+                            size: 30,
+                          ),
+                          Text(bssid)
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
